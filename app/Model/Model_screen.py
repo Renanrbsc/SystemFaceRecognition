175ae3783c07e09f8678b7a61aa8504bffa96044
Renan Berti da Cruz Ribas <renan.ribas@hbsis.com.br>
2020-04-28 15:36:23 -0300

    organizing class

--- app/Model/Model_screen.py
+++ app/Model/Model_screen.py
@@ -12,25 +12,14 @@ class Screen:
 
     def read(self):
         retval, self.image = self.video_capture.read()
-        return self.image
 
     def redimension_image(self):
         self.image = cv2.flip(self.image, 0)  # Flip vertically
         self.image = cv2.rotate(self.image, cv2.ROTATE_90_COUNTERCLOCKWISE)  # Rotate vertically
-
-    def detection_color(self):
-        color = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        return color
+        return self.image
 
     def title_screen(self):
         cv2.imshow('Facial Recognition System', self.image)
-
-    def format_rectangle(self, x, y, w, h):
-        cv2.rectangle(self.image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    def detection_rectangle(self, rectangle: list, x, y, w, h):
-        new_rectangle = rectangle[y:y + h, x:x + w]
-        return new_rectangle
 
     def close_screen(self):
         if cv2.waitKey(30) == 27:
